a1283d78bc3ec300ed88841b51430562e07d8a4f
Mario Gielissen <mario@gielissen.org>
2015-07-28 10:46:26 -0400

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Theme Worthy",
+
+    'summary': """
+    Custom theme""",
+
+    'description': """
+    Theme Worthy https://github.com/Html-Coder/Worthy""",
+
+    'author': "Mario Gielissen",
+    'website': "http://www.openworx.nl",
+
+    'category': 'Theme',
+    'version': '1.0',
+
+    'depends': [
+        'base',
+        'website'
+    ],
+    'data': [
+        'views/layout.xml',
+        'views/assets.xml'
+    ],
+
+    'demo': [
+    ],
+
+    'tests': [
+    ],
+}
