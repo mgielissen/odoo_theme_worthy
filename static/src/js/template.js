725c3a69c4dd43e740bfd5605ffb4f0c3ba76715
Mario Gielissen <mario@gielissen.org>
2015-07-30 06:01:45 -0400

    new page and edit menu fix

--- static/src/js/template.js
+++ static/src/js/template.js
@@ -10,7 +10,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+/*		$(".banner-image").backstretch('images/banner.jpg'); */
 		
 		// Fixed header
 		//-----------------------------------------------
